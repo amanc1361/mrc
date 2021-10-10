4d18ed03425fc91db241aec3aeb09477f248c0bf
amanc1361 <rahymrm@gmail.com>
2021-10-10 22:00:57 +0330

    change

--- pages/index.tsx
+++ pages/index.tsx
@@ -91,7 +91,7 @@ const Home: NextPage = () => {
   
      <Head>
     
-     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css" integrity="sha384-gXt9imSW0VcJVHezoNQsP+TNrjYXoGcrqBZJpry9zJt8PCQjobwmhMGaDHTASo9N" crossorigin="anonymous"/>
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css" integrity="sha384-gXt9imSW0VcJVHezoNQsP+TNrjYXoGcrqBZJpry9zJt8PCQjobwmhMGaDHTASo9N" crossOrigin="anonymous"/>
      </Head>
     <div className="container">
     <main>
